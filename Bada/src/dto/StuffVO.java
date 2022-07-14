b2bbeee0bc73046d6bc35986377b22a5751896ec
ddHyun <ddHyun@github.com>
2022-07-14 14:03:45 +0900

    docs: OrderPage2

---
+++ Bada/src/dto/StuffVO.java
@@ -0,0 +1,45 @@
+package dto;
+
+public class StuffVO {//주문물품 관련 정보
+	private int size, weight, box;
+	private String code, note, stuff;
+	
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public int getBox() {
+		return box;
+	}
+	public void setBox(int box) {
+		this.box = box;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+	public String getStuff() {
+		return stuff;
+	}
+	public void setStuff(String stuff) {
+		this.stuff = stuff;
+	}
+	
+	
+}
--- Bada/src/view/OrderPage3.java
+++ Bada/src/view/OrderPage3.java
@@ -8,6 +8,7 @@ import dao.ImageDAO;
 import dao.LabelDAO;
 import dao.PanelDAO;
 import dto.FrameVO;
+import dto.StuffVO;
 
 public class OrderPage3 {
 
@@ -29,8 +30,20 @@ public class OrderPage3 {
 		titleLabel = new LabelDAO("주문확인&결제", FrameVO.font40, order3Layer, 1);
 		titleLabel.makeTitleLabel();
 		
-		receiverInfo = new OrderPage().receiverInfo();		
-			System.out.println(receiverInfo);
+		receiverInfo = new OrderPage().receiverInfo();	
+//		StuffVO stuffVO = new OrderPage2().getStuffInfo();
+//		
+//			System.out.println("수령자 이름: "+receiverInfo.get(0));
+//			System.out.println("수령자 전화번호: "+receiverInfo.get(1));
+//			System.out.println("수령자 주소: "+receiverInfo.get(2));
+//			System.out.println("물품 크기: "+stuffVO.getSize());
+//			System.out.println("물품 무게: "+stuffVO.getWeight());
+//			System.out.println("물품 코드: "+stuffVO.getCode());
+//			System.out.println("물품 내용물: "+stuffVO.getStuff());
+//			System.out.println("물품 박스갯수: "+stuffVO.getBox());
+//			System.out.println("물품 주의사항: "+stuffVO.getNote());
+			
+			
 	
 		
 		//아이콘이미지
